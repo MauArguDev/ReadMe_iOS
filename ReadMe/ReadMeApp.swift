f0cb1fcfb1428c0c2b02c5e42f6c15e0c7412fbf
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2021-06-25 13:05:52 -0500

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by user199344 on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
